7c9bebea73e60d786a09a79194a41b61b30ca5e3
MarvinB97 <mb.jobtoday@gmail.com>
2023-04-23 13:04:40 -0500

    001 23-04-23

---
+++ clase1.h
@@ -0,0 +1,139 @@
+/*
+Author: Marvin Brayan Nuñez Zuñiga
+Date: 23/04/23
+*/
+
+//------------------TEORIA-------------------------
+
+/*
+1. ¿Qué es la estadística?
+
+
+
+1.1. Conceptos
+
+Aunque para diversos ámbitos la palabra estadística se use de forma diferente, formalmente se puede decir que “es la ciencia que se encarga de la recolección, organización, análisis e interpretación de datos para tomar decisiones en ambientes de incertidumbre”.
+
+Principalmente la estadística proporciona herramientas (métodos y técnicas) para sacar
+conclusiones de diferentes características (variables) de un gran número de datos (población) usando un subconjunto de ellos (muestra).
+
+Las técnicas estadísticas ayudan a identificar, describir y entender diferentes fuentes de
+variabilidad (resultados de un mismo experimento u observaciones de un mismo fenómeno no son siempre iguales).
+
+La variabilidad parece estar presente en todo momento y el pensamiento estadístico nos ayuda a tomar decisiones en medio de la incertidumbre debida a esa variabilidad.
+
+PENSAMIENTO ESTADÌSTICO:
+La estadística es una disciplina en la cual la parte intenta hablar por el todo...
+Existe algo que puede describir el comportamiento de los humanos aun cuando
+fallan las ecuaciones... LOS DATOS
+
+
+1.2. ¿Que son Datos?
+
+•	Los datos son información o hechos que se recopilan y se almacenan en una forma estructurada o no estructurada.
+•	Pueden ser cualquier elemento o característica de interés, desde números y estadísticas hasta texto, imágenes y videos….
+•	Los datos son una representación de la realidad que se puede utilizar para comprender y mejorar el mundo que nos rodea.
+
+
+
+
+
+
+
+2. Tipos de Variables
+
+
+
+Población y Muestra
+
+Se denomina población al conjunto completo de elementos, con alguna característica común, que es el objeto de nuestro estudio. Esta definición incluye, por ejemplo, a todos los sucesos en que podría concretarse un fenómeno o experimento cualesquiera. Una población puede ser finita o infinita.
+
+Cuando, aunque la población sea finita, su número de elementos es elevado, es necesario trabajar con solo una parte de dicha población. A un subconjunto de elementos de la población se le conoce como muestra.
+
+El objeto de nuestra medida pueden ser caracteres de tipos muy diversos. De ahí que normalmente se clasifiquen en:
+
+•	Caracteres cuantitativos: aquellos que toman valores numéricos. Por ejemplo, la altura o la velocidad de un automóvil.
+•	Caracteres cualitativos: también llamados atributos, son aquellos que no podemos representar numéricamente y describen cualidades. Por ejemplo, un color o el estado civil.
+
+Aunque existen algunas diferencias, el tratamiento para ambos casos es similar, pudiéndose asignar, en muchas ocasiones, valores numéricos a los diferentes caracteres cualitativos.
+
+ 
+TIPOS DE VARIABLE={cualitativas,cuantitativas}
+se clasifican en {{Nominal,Ordinal},{Discretas, Continuas}}
+
+
+Variables Cualitativas
+
+Los valores que puede asumir representan cualidades y/o atributos, por lo tanto no se pueden
+realizar operaciones algebraicas.
+Tipo Nominal : La variable se divide en categorías o modalidades (Grupos). No existe una
+forma particular para ordenar las posibles categorías (No hay orden natural).
+Tipo Ordinal : La variable se divide en categorías o modalidades, las cuales tienen valores
+relativos (Por ejemplo; alto, medio, bajo). Existe una forma particular para ordenar las posibles
+categorías (Orden natural o Jerarquía).
+
+
+Variables Cuantitativa
+
+Los valores que puede asumir son numéricos (Cantidades o mediciones) y tiene sentido realizar
+operaciones algebraicas con ellos.
+
+Tipo Discreta : Pueden tomar valores que estarán siempre asociados a números enteros
+(Existen vacíos entre ellos). Por lo general, son respuestas numéricas que surgen de procesos
+de conteo.
+Tipo Continua : Pueden tomar valores que estarán contenidos siempre entre dos valores
+cualesquiera (Intervalo). Por lo general las variables continuas son el resultado de mediciones.
+
+
+Variable Estadística:
+Se entiende por variable estadística al símbolo que representa al dato o carácter
+objeto de nuestro estudio de los elementos de la muestra y que puede tomar un
+conjunto de valores.
+
+
+
+Validez y Confiabilidad
+La validez es el grado de valor que se confiere a la información
+-La Validez Interna hace referencia al grado en que la medición refleja la situación que se pretende medir.
+Garantizar la comparabilidad.
+-La Validez Externa hace referencia al grado en que la medición puede generalizarse a otras situaciones no medidas.
+Garantizar la representatividad.
+
+
+
+
+*ERROR SISTEMATICO == SESGO
+
+
+
+
+
+
+
+Ejercicio
+Clasifique las siguientes variables según será el tipo: Cualitativa (Nominal Ordinal) o
+Cuantitativa (Continua o discreta).
+a. Número de personas que pasan por un semáforo.
+b. Ingreso mensual de un profesional.
+c. Color de vehículos que llegan a un estacionamiento.
+d. Estrato socioeconómico registrado en una encuesta.
+e. Número de páginas por libros.
+f. Temperatura de la ciudad de Cali hoy
+g. Puesto obtenido por equipo en la Copa América 2019.
+
+
+
+
+
+RESPUESTAS:
+
+a. Es de tipo cuantitativa y escala discreta
+b. -----------cuantitativa ---------continua
+c. -----------cualitativa ----------nominal
+d. -----------cualitativa ----------ordinal
+e. -----------cuantitativa ---------discreta
+f. -----------cuantitativa ---------continua
+g. -----------cualitativa ----------ordinal
+
+
+*/
